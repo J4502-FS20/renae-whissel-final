eb3810c4a871bcbb6a2fac68ac50f3d6e10dedb2
Renae Whissel <70670671+renaewhissel@users.noreply.github.com>
2020-12-12 20:51:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,65 +1,13 @@
-/*jshint esversion: 6 */
 "use strict";
 
-var photoDivs = document.getElementsByClassName("imageHolder");
+var selectedImg = document.getElementById("selected_img");
 
-var nextButton = document.getElementById("button_next");
+var images = document.getElementsByClassName("image");
 
-var previousButton = document.getElementById("button_previous");
+selectedImg.innerHTML = images[0].innerHTML;
 
-
-var currentPhotoNumber = 0;
-
-photoDivs[currentPhotoNumber].classList.remove('hideThisDiv');
-
-nextButton.addEventListener('click', function () {
-    photoDivs[currentPhotoNumber].classList.add("hideThisDiv");
-    currentPhotoNumber = currentPhotoNumber + 1;
-
-    if (currentPhotoNumber === photoDivs.length) {
-        currentPhotoNumber = 0;
-    }
-
-    photoDivs[currentPhotoNumber].classList.remove('hideThisDiv');
-
-previousButton.addEventListener('click', function () {
-    photoDivs[currentPhotoNumber].classList.add("hideThisDiv");
-    currentPhotoNumber = currentPhotoNumber - 1;
-
-    if (currentPhotoNumber < 0) {
-        currentPhotoNumber = photoDivs.length - 1;
-    }
-
-    photoDivs[currentPhotoNumber].classList.remove('hideThisDiv');
-
-var photoDivs = document.getElementsByClassName("imageHolder");
-
-var nextButton = document.getElementById("button_next");
-
-var previousButton = document.getElementById("button_previous");
-
-var currentPhotoNumber = 0;
-
-photoDivs[currentPhotoNumber].classList.remove('hideThisDiv');
-
-nextButton.addEventListener('click', function() {
-  photoDivs[currentPhotoNumber].classList.add("hideThisDiv");
-  currentPhotoNumber = currentPhotoNumber + 1;
-
-  if (currentPhotoNumber === photoDivs.length) {
-    currentPhotoNumber = 0;
-  }
-
-  photoDivs[currentPhotoNumber].classList.remove('hideThisDiv');
-
-previousButton.addEventListener('click', function() {
-  photoDivs[currentPhotoNumber].classList.add("hideThisDiv");
-  currentPhotoNumber = currentPhotoNumber - 1;
-
-  if (currentPhotoNumber < 0) {
-    currentPhotoNumber = photoDivs.length - 1;
-  }
-
-  photoDivs[currentPhotoNumber].classList.remove('hideThisDiv');
-
-});
+for (var i = 0; i < images.length; i++) {
+  images[i].addEventListener("click", function() {
+    selectedImg.innerHTML = this.innerHTML;
+  });
+}

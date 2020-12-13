a60e55192590fa8008ce08996adc014d0acc5d70
Renae Whissel <70670671+renaewhissel@users.noreply.github.com>
2020-12-13 17:10:24 -0400

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -9,37 +9,5 @@ selectedImg.innerHTML = images[0].innerHTML;
 for (var i = 0; i < images.length; i++) {
   images[i].addEventListener("click", function() {
     selectedImg.innerHTML = this.innerHTML;
-
-var photoDivs = document.getElementsByClassName("imageHolder");
-
-var nextButton = document.getElementById(button_next);
-
-var previousButton = docomunt.getElementById(button_previous);
-
-var currentPhotoNumber = currentPhotoNumber;
-
-photoDivs[currentPhotoNumber].classList.remove('hideThisDiv');
-
-nextButton.addEventListener('click', function() {
-  photoDivs[currentPhotoNumber].classList.add(hideThisDiv)
-  currentPhotoNumber = currentPhotoNumber + 1;
-
-  console.log(currentPhotoNumber);
-
-  if (currentPhotoNumber === photoDivs.length){
-    curentPhotoNumber = 0;
-  }
-  photoDivs[currentPhotoNumber].classList.remove('hideThisDiv');
-});
-
-previosButton.addEventListener('click', function() {
-  photoDivs[currentPhotoNumber].classList.add("hideThisDiv");
-  currentPhotoNumber - 1;
-
-  if(currentPhotoNumber <0) {
-    currentPhotoNumber = photoDivs.length - 1;
-  }
-  photoDivs[currentPhotoNumber].classList.remove('hideThisDiv')
-})
   });
 }

c9552f1a391dc8f2b7b986551f747f9cd111de96
Renae Whissel <70670671+renaewhissel@users.noreply.github.com>
2020-12-13 16:48:45 -0400

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -18,14 +18,25 @@ var currentPhotoNumber = currentPhotoNumber;
 
 photoDivs[currentPhotoNumber].classList.remove('hideThisDiv');
 
-nextButton.addEventListener('click', function()) {
+nextButton.addEventListener('click', function() {
   photoDivs[currentPhotoNumber].classList.add(hideThisDiv)
   currentPhotoNumber = currentPhotoNumber + 1;
 
   if (currentPhotoNumber === photoDivs.length){
     curentPhotoNumber = 0;
   }
-  photoDivs[currentPhotoNumber.classList.add]
-}
+  photoDivs[currentPhotoNumber.classList.remove('hideThisDiv')]
+});
+
+previosButton.addEventListener('click', function() {
+  photoDivs[currentPhotoNumber].classList.add("hideThisDiv");
+  currentPhotoNumber - 1;
+
+  if(currentPhotoNumber <0) {
+    currentPhotoNumber = photoDivs.length - 1;
+  }
+
+  photoDivs[currentPhotoNumber].classList.remove('hideThisDiv')
+})
   });
 }

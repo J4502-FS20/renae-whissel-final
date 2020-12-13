6f85b33e63977802181383b7d26aa2b6d00a2a22
Renae Whissel <70670671+renaewhissel@users.noreply.github.com>
2020-12-13 16:26:57 -0400

    Create gallery.js

---
+++ gallery.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var selectedImg = document.getElementById("selected_img");
+
+var images = document.getElementsByClassName("image");
+
+selectedImg.innerHTML = images[0].innerHTML;
+
+for (var i = 0; i < images.length; i++) {
+  images[i].addEventListener("click", function() {
+    selectedImg.innerHTML = this.innerHTML;
+  });
+}
